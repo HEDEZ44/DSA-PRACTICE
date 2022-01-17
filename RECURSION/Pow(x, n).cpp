1aff1fa8d24bed444962d8b0fe0b8bdcdc632572
HEDEZ <sanjaykumar2222267@gmail.com>
2022-01-17 16:23:25 +0530

    power function using recursion

---
+++ RECURSION/Pow(x, n).cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        
+        if(n==0)
+            return 1;
+        
+        
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        double ans=myPow(x,n/2);
+        ans*=ans;
+        if(n%2==1){
+            if(n>0)
+                ans*=x;
+        }
+            
+        return ans;
+        cout<<ans<<endl;;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
