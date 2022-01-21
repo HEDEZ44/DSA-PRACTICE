0eb222d34e672626bdaccc7aa0124955405c04c9
HEDEZ <sanjaykumar2222267@gmail.com>
2022-01-21 22:49:27 +0530

    maps and others

---
+++ MAPS/First Repeating Element.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the position of the first repeating element.
+    int firstRepeated(int arr[], int n) {
+        
+        unordered_map<int,int>mp;
+    
+    for(int i=0;i<n;i++){
+        if(mp.find(arr[i])==mp.end()){
+            mp[arr[i]]=1;
+        }
+        else{
+            mp[arr[i]]++;
+        }
+    }
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(mp[arr[i]]>1){
+            j=i;
+            return j+1;
+        }
+        
+        
+        
+    }
+    return -1;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; ++i) cin >> arr[i];
+        Solution ob;
+        cout << ob.firstRepeated(arr, n) << "\n";
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
