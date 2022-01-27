2ce5385340f6ece17d07a3b221142d926de8ba0f
HEDEZ <sanjaykumar2222267@gmail.com>
2022-01-27 16:45:08 +0530

    also look for constraints

---
+++ BACKTRACKING/Maximal_string.cpp
@@ -0,0 +1,26 @@
+void maxstring(string A, int B, string &temp){
+    if(B==0){
+        return;
+    }
+    for(int i=0;i<A.length()-1;i++){
+        for(int j=i+1;j<A.length();j++){
+            if(A[i]<A[j]){
+                swap(A[i],A[j]);
+                if(A.compare(temp)>0){
+                    temp=A;
+                }
+                maxstring(A,B-1,temp);
+                swap(A[i],A[j]);
+
+            }
+        }
+    }
+}
+
+
+
+string Solution::solve(string A, int B) {
+    string temp=A;
+    maxstring(A,B,temp);
+    return temp;
+}
