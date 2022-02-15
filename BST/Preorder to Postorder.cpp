2f392adab71d9bba772dd8c54991a2a4be71cfc7
HEDEZ <sanjaykumar2222267@gmail.com>
2022-02-15 19:52:53 +0530

    median of bst

--- BST/Preorder to Postorder.cpp
+++ BST/Preorder to Postorder.cpp
@@ -1,19 +1,18 @@
-// { Driver Code Starts
-//Initial template for C++
 
-/* A O(n) iterative program for construction of BST from preorder traversal */
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child */
+
 typedef struct Node
 {
     int data;
     struct Node *left, *right;
 } Node;
 
-// A Stack has array of Nodes, capacity, and top
+
 typedef struct Stack
 {
     int top;
@@ -21,7 +20,7 @@ typedef struct Stack
     Node* *array;
 } Stack;
 
-// A utility function to create a new tree node
+
 Node* newNode( int data )
 {
     Node* temp = (Node *)malloc( sizeof( Node ) );
@@ -30,7 +29,7 @@ Node* newNode( int data )
     return temp;
 }
 
-// A utility function to create a stack of given capacity
+
 Stack* createStack( int capacity )
 {
     Stack* stack = (Stack *)malloc( sizeof( Stack ) );
@@ -40,19 +39,19 @@ Stack* createStack( int capacity )
     return stack;
 }
 
-// A utility function to check if stack is full
+
 int isFull( Stack* stack )
 {
     return stack->top == stack->capacity - 1;
 }
 
-// A utility function to check if stack is empty
+
 int isEmpty( Stack* stack )
 {
     return stack->top == -1;
 }
 
-// A utility function to push an item to stack
+
 void push( Stack* stack, Node* item )
 {
     if( isFull( stack ) )
@@ -60,7 +59,7 @@ void push( Stack* stack, Node* item )
     stack->array[ ++stack->top ] = item;
 }
 
-// A utility function to remove an item from stack
+
 Node* pop( Stack* stack )
 {
     if( isEmpty( stack ) )
@@ -68,7 +67,7 @@ Node* pop( Stack* stack )
     return stack->array[ stack->top-- ];
 }
 
-// A utility function to get top node of stack
+
 Node* peek( Stack* stack )
 {
     return stack->array[ stack->top ];
@@ -76,40 +75,40 @@ Node* peek( Stack* stack )
 
 bool canRepresentBST(int pre[], int n)
 {
-    // Create an empty stack
+    
     stack<int> s;
 
-    // Initialize current root as minimum possible
-    // value
+    
+    
     int root = INT_MIN;
 
-    // Traverse given array
+    
     for (int i=0; i<n; i++)
     {
-        // If we find a node who is on right side
-        // and smaller than root, return false
+        
+        
         if (pre[i] < root)
             return false;
 
-        // If pre[i] is in right subtree of stack top,
-        // Keep removing items smaller than pre[i]
-        // and make the last removed item as new
-        // root.
+        
+        
+        
+        
         while (!s.empty() && s.top()<pre[i])
         {
             root = s.top();
             s.pop();
         }
 
-        // At this point either stack is empty or
-        // pre[i] is smaller than root, push pre[i]
+        
+        
         s.push(pre[i]);
     }
     return true;
 }
 
 
-// A utility function to print postorder traversal of a Binary Tree
+
 void printPostorder (Node* node)
 {
     if (node == NULL)
@@ -119,25 +118,25 @@ void printPostorder (Node* node)
     printf("%d ", node->data);
 }
 
-// The main function that constructs BST from pre[]
+
 Node* post_order (int *, int );
 
 int main()
 {
-    // Note that size of arr[] is considered 100 according to
-    // the constraints mentioned in problem statement.
+    
+    
     int arr[1000], x, t, n;
 
-    // Input the number of test cases you want to run
+    
     cin >> t;
 
-    // One by one run for all input test cases
+    
     while (t--)
     {
-        // Input the size of the array
+        
         cin >> n;
 
-        // Input the array
+        
         for (int i=0; i<n; i++)
             cin >> arr[i];
 
@@ -146,13 +145,13 @@ int main()
     }
     return 0;
 }
-// } Driver Code Ends
 
 
-//User function template in C++
 
 
-//Function that constructs BST from its preorder traversal.
+
+
+
 Node* preorder(int pre[], int size, int &idx, int &min, int &max){
     if(idx>=size) return NULL;
     Node* root=NULL;
@@ -168,7 +167,7 @@ Node* preorder(int pre[], int size, int &idx, int &min, int &max){
 }
 Node* post_order(int pre[], int size)
 {
-    //code here
+    
     int idx=0;
     int min=INT_MIN;
     int max=INT_MAX;
