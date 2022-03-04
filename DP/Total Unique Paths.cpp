1771125e64af9686753cecbbb39307c05b89d6d5
HEDEZ <sanjaykumar2222267@gmail.com>
2022-03-04 20:33:41 +0530

    O(1) space complexity

--- DP/Total Unique Paths.cpp
+++ DP/Total Unique Paths.cpp
@@ -23,3 +23,18 @@ int uniquePaths(int m, int n)
     }
     return prev[n - 1];
 }
+
+// O(1) space complexity
+
+int uniquePaths(int m, int n)
+{
+    // Write your code here.
+    int N = m + n - 2;
+    int r = m - 1;
+    double res = 1;
+    for (int i = 1; i <= r; i++)
+    {
+        res = res * (N - r + i) / i;
+    }
+    return (int)res;
+}
