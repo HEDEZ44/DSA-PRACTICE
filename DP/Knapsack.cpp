637bedfab0446e676e18266be761f56e57bd787e
HEDEZ <sanjaykumar2222267@gmail.com>
2022-03-31 02:36:28 +0530

    knapsack

---
+++ DP/Knapsack.cpp
@@ -0,0 +1,23 @@
+int knapsack(vector<int> wt, vector<int> val, int n, int maxWeight)
+{
+    // Write your code here
+    vector<vector<int>> dp(n, vector<int>(maxWeight + 1, 0));
+
+    vector<int> prev(maxWeight + 1, 0);
+    for (int W = wt[0]; W <= maxWeight; W++)
+        prev[W] = val[0];
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int W = maxWeight; W >= 0; W--)
+        {
+            int notTake = prev[W];
+            int take = INT_MIN;
+            if (wt[ind] <= W)
+            {
+                take = val[ind] + prev[W - wt[ind]];
+            }
+            prev[W] = max(take, notTake);
+        }
+    }
+    return prev[maxWeight];
+}
